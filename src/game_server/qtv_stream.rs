1e4f6f506d942629ebe13433843b3ce45f096b08
Viktor Persson <viktor.persson@arcsin.se>
2025-05-26 18:08:00 +0200

    refactor QtvStream

--- src/game_server/qtv_stream.rs
+++ src/game_server/qtv_stream.rs
@@ -1,6 +1,3 @@
-use crate::net::tokenize::tokenize;
-use quake_text::bytestr::to_unicode;
-
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
 
@@ -8,12 +5,12 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Default)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct QtvStream {
-    id: u32,
-    name: String,
-    number: Option<u32>,
-    address: Option<String>,
-    client_count: u32,
-    pub(super) client_names: Vec<String>,
+    pub(crate) id: u32,
+    pub(crate) name: String,
+    pub(crate) number: Option<u32>,
+    pub(crate) address: Option<String>,
+    pub(crate) client_count: u32,
+    pub(crate) client_names: Vec<String>,
 }
 
 #[allow(dead_code)]
@@ -43,74 +40,6 @@ impl QtvStream {
     }
 
     pub fn url(&self) -> Option<String> {
-        match (self.number(), self.address()) {
-            (Some(number), Some(address)) => Some(format!("{}@{}", number, address)),
-            _ => None,
-        }
-    }
-}
-
-impl TryFrom<&[u8]> for QtvStream {
-    type Error = anyhow::Error;
-
-    fn try_from(bytes: &[u8]) -> Result<Self, Self::Error> {
-        // todo validate number of parts
-        let parts: Vec<String> = tokenize(to_unicode(bytes).as_str());
-        let id = parts[1].parse::<u32>().unwrap_or_default();
-        let name = parts[2].to_string();
-        let url = parts[3].to_string();
-        let (number, address) = url
-            .split_once('@')
-            .map(|(num_str, addr)| (num_str.parse::<u32>().ok(), Some(addr.to_string())))
-            .unwrap_or((None, None));
-        let client_count = parts[4].parse::<u32>().unwrap_or_default();
-
-        Ok(Self {
-            id,
-            name,
-            number,
-            address,
-            client_count,
-            client_names: vec![],
-        })
-    }
-}
-
-#[cfg(test)]
-#[cfg_attr(coverage_nightly, coverage(off))]
-mod tests {
-    use super::*;
-    use anyhow::Result;
-    use pretty_assertions::assert_eq;
-
-    #[test]
-    fn test_try_from_bytes() -> Result<()> {
-        // empty url
-        {
-            let bytes = br#"qtv 2 "QUAKE.SE KTX Qtv (1)" "" 0"#.as_slice();
-            let stream = QtvStream::try_from(bytes)?;
-            assert_eq!(stream.id(), 2);
-            assert_eq!(stream.name(), "QUAKE.SE KTX Qtv (1)");
-            assert_eq!(stream.number(), None);
-            assert_eq!(stream.address(), None);
-            assert_eq!(stream.client_count(), 0);
-            assert!(stream.client_names.is_empty());
-            assert_eq!(stream.url(), None);
-        }
-
-        // valid
-        {
-            let bytes = br#"qtv 2 "QUAKE.SE KTX Qtv (1)" "1@quake.se:28000" 0"#.as_slice();
-            let stream = QtvStream::try_from(bytes)?;
-            assert_eq!(stream.id(), 2);
-            assert_eq!(stream.name(), "QUAKE.SE KTX Qtv (1)");
-            assert_eq!(stream.number(), Some(1));
-            assert_eq!(stream.address(), Some(&"quake.se:28000".to_string()));
-            assert_eq!(stream.client_count(), 0);
-            assert!(stream.client_names.is_empty());
-            assert_eq!(stream.url(), Some("1@quake.se:28000".to_string()));
-        }
-
-        Ok(())
+        Some(format!("{}@{}", self.number()?, self.address()?))
     }
 }
