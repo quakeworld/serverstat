398acd46ca2d2f1f20d330e5b4c4f9148e9ecd47
Viktor Persson <viktor.persson@arcsin.se>
2025-05-26 17:55:49 +0200

    refactor hostname resolution

--- src/net/resolve.rs
+++ src/net/resolve.rs
@@ -1,16 +1,24 @@
-use anyhow::{Result, anyhow as e};
 use std::net::{SocketAddr, ToSocketAddrs};
 
-/// Resolves a hostname or IP string to an IPv4 address.
-pub(super) fn resolve_host(host: &str) -> Result<String> {
+/// Resolves a hostname or IP string to its first IPv4 address.
+pub(super) fn host_as_ipv4(host: &str) -> Result<String, Error> {
     (host, 0)
         .to_socket_addrs()
-        .map_err(|_| e!("Failed to resolve '{}' to an IPv4 address.", host))?
+        .map_err(|_| Error::FailedToResolve(host.to_string()))?
         .find_map(|addr| match addr {
             SocketAddr::V4(v4) => Some(v4.ip().to_string()),
             _ => None,
         })
-        .ok_or_else(|| e!("Failed to resolve '{}' to an IPv4 address.", host))
+        .ok_or_else(|| Error::FailedToResolve(host.to_string()))
+}
+
+#[derive(Debug, PartialEq, thiserror::Error)]
+pub enum Error {
+    #[error(transparent)]
+    InvalidFormat(#[from] hostport::ParseError),
+
+    #[error("failed to resolve '{0}' to an IPv4 address")]
+    FailedToResolve(String),
 }
 
 #[cfg(test)]
@@ -21,13 +29,19 @@ pub mod tests {
     use pretty_assertions::assert_eq;
 
     #[tokio::test]
-    async fn test_resolve_host() -> Result<()> {
-        assert!(resolve_host("INVALID_ADDRESS").is_err());
-        assert!(resolve_host("0:0:0:0:0:ffff:c0a8:0001:28000").is_err());
-        assert_eq!(resolve_host("1.2.3.4")?, "1.2.3.4".to_string());
+    async fn test_host_as_ipv4() -> Result<()> {
+        assert_eq!(
+            host_as_ipv4("INVALID_ADDRESS").unwrap_err(),
+            Error::FailedToResolve("INVALID_ADDRESS".to_string())
+        );
+        assert_eq!(
+            host_as_ipv4("0:0:0:0:0:ffff:c0a8:0001:28000").unwrap_err(),
+            Error::FailedToResolve("0:0:0:0:0:ffff:c0a8:0001:28000".to_string())
+        );
+        assert_eq!(host_as_ipv4("1.2.3.4")?, "1.2.3.4".to_string());
         assert!(
             ["1.1.1.1".to_string(), "1.0.0.1".to_string()]
-                .contains(&resolve_host("one.one.one.one")?)
+                .contains(&host_as_ipv4("one.one.one.one")?)
         );
         Ok(())
     }
