e35d0d5b4ee0767f0de196b5dc3f7ac02b6d0b01
Viktor Persson <viktor.persson@arcsin.se>
2025-05-26 17:04:54 +0200

    refactor svc_qtvusers

--- src/net/svc_qtvusers.rs
+++ src/net/svc_qtvusers.rs
@@ -1,48 +1,43 @@
 use super::tokenize::tokenize;
-use anyhow::Result;
 use quake_text::bytestr;
 use std::time::Duration;
-use thiserror::Error;
 use tinyudp::ReadOptions;
 
 /// Sends a `qtvusers` request to the specified address and returns the response.
-// https://github.com/QW-Group/mvdsv/blob/master/src/sv_demo_qtv.c#L1379
-const CMD_QTVUSERS: &[u8] = b"\xff\xff\xff\xffqtvusers";
-const BUFFER_SIZE: usize = 4 * 1024;
+/// https://github.com/QW-Group/mvdsv/blob/master/src/sv_demo_qtv.c#L1379
+const REQUEST_MESSAGE: &[u8] = b"\xff\xff\xff\xffqtvusers";
+const REQUEST_BUFFER_SIZE: usize = 4 * 1024;
+const EXPECTED_RESPONSE_HEADER: &[u8] = b"\xff\xff\xff\xffnqtvusers ";
 
-pub(super) fn qtvusers(
-    address: &str,
-    timeout: Duration,
-) -> Result<QtvusersResponse, QtvusersResponseError> {
-    let bytes = tinyudp::send_and_receive(
+pub(super) fn query_qtvusers(address: &str, timeout: Duration) -> Result<QtvusersResponse, Error> {
+    let response = tinyudp::send_and_receive(
         address,
-        CMD_QTVUSERS,
-        ReadOptions::new(timeout, BUFFER_SIZE),
-    )
-    .map_err(|e| QtvusersResponseError::UdpError(e.to_string()))?;
-    QtvusersResponse::try_from(bytes.as_slice())
+        REQUEST_MESSAGE,
+        ReadOptions::new(timeout, REQUEST_BUFFER_SIZE),
+    )?;
+    Ok(QtvusersResponse::parse(&response)?)
 }
 
+/// Async version
 #[cfg(feature = "tokio")]
-pub(super) async fn qtvusers_async(
+pub(super) async fn query_qtvusers_async(
     address: &str,
     timeout: Duration,
-) -> Result<QtvusersResponse, QtvusersResponseError> {
-    let bytes = tinyudp::send_and_receive_async(
+) -> Result<QtvusersResponse, Error> {
+    let response = tinyudp::send_and_receive_async(
         address,
-        CMD_QTVUSERS,
-        ReadOptions::new(timeout, BUFFER_SIZE),
+        REQUEST_MESSAGE,
+        ReadOptions::new(timeout, REQUEST_BUFFER_SIZE),
     )
-    .await
-    .map_err(|e| QtvusersResponseError::UdpError(e.to_string()))?;
-    QtvusersResponse::try_from(bytes.as_slice())
+    .await?;
+    Ok(QtvusersResponse::parse(&response)?)
 }
 
 /// Parses the response from a `qtvusers` request.
 #[derive(Debug, Clone, PartialEq, Eq, Default)]
 pub(super) struct QtvusersResponse {
     /// The stream ID of the server.
-    stream_id: usize,
+    stream_id: u32,
 
     /// The names of the clients connected to the server.
     client_names: Vec<String>,
@@ -50,61 +45,60 @@ pub(super) struct QtvusersResponse {
 
 #[allow(dead_code)]
 impl QtvusersResponse {
-    pub fn stream_id(&self) -> usize {
+    pub fn stream_id(&self) -> u32 {
         self.stream_id
     }
 
-    pub fn client_names(&self) -> Vec<String> {
-        self.client_names.clone()
+    pub fn client_names(&self) -> &[String] {
+        &self.client_names
     }
-}
 
-impl TryFrom<&[u8]> for QtvusersResponse {
-    type Error = QtvusersResponseError;
-
-    fn try_from(bytes: &[u8]) -> Result<Self, Self::Error> {
+    pub fn parse(response: &[u8]) -> Result<Self, ParseError> {
         // validate header
-        let header = b"\xff\xff\xff\xffnqtvusers ".to_vec();
-
-        if !bytes.starts_with(&header) {
-            return Err(QtvusersResponseError::InvalidHeader);
+        if !response.starts_with(EXPECTED_RESPONSE_HEADER) {
+            return Err(ParseError::InvalidHeader);
         }
 
         // extract body
         let body = {
-            let Some(end_pos) = bytes.iter().position(|&b| b == b'\n') else {
-                return Err(QtvusersResponseError::InvalidBody(
-                    "No newline found".to_string(),
-                ));
+            let Some(end_pos) = response.iter().position(|&b| b == b'\n') else {
+                return Err(ParseError::InvalidBody("missing newline".to_string()));
             };
-            &bytes[header.len()..end_pos]
+            &response[EXPECTED_RESPONSE_HEADER.len()..end_pos]
         };
 
         // parse body
-        let parts = tokenize(&bytestr::to_unicode(body));
-        let stream_id = parts[0].parse::<usize>()?;
-        let client_names = parts[1..].iter().map(|s| s.to_string()).collect();
+        let tokens = tokenize(&bytestr::to_unicode(body));
 
-        Ok(Self {
-            stream_id,
-            client_names,
+        let Some((first, rest)) = tokens.split_first() else {
+            return Err(ParseError::InvalidBody("missing stream id".to_string()));
+        };
+
+        Ok(QtvusersResponse {
+            stream_id: first.parse::<u32>().map_err(|_| {
+                ParseError::InvalidBody("invalid stream id: not a number".to_string())
+            })?,
+            client_names: rest.iter().map(|s| s.to_string()).collect(),
         })
     }
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, Error)]
-pub enum QtvusersResponseError {
-    #[error("UDP error: {0}")]
-    UdpError(String),
+#[derive(Debug, thiserror::Error)]
+pub(crate) enum Error {
+    #[error("query error: {0}")]
+    QueryError(#[from] tinyudp::Error),
 
-    #[error("Invalid response header")]
+    #[error(transparent)]
+    ParseError(#[from] ParseError),
+}
+
+#[derive(Debug, thiserror::Error, PartialEq, Eq, Clone)]
+pub(crate) enum ParseError {
+    #[error("invalid response header")]
     InvalidHeader,
 
-    #[error("Invalid response body")]
+    #[error("invalid response body: {0}")]
     InvalidBody(String),
-
-    #[error("Invalid stream id")]
-    InvalidStreamId(#[from] std::num::ParseIntError),
 }
 
 #[cfg(test)]
@@ -118,63 +112,57 @@ mod tests {
     async fn test_qtvusers() -> Result<()> {
         // invalid address
         assert_eq!(
-            qtvusers_async("INVALID_ADDRESS", Duration::from_secs(1))
+            query_qtvusers_async("INVALID_ADDRESS", Duration::from_secs(1))
                 .await
-                .unwrap_err(),
-            QtvusersResponseError::UdpError(
-                "failed to send message: invalid socket address".to_string()
-            )
+                .unwrap_err()
+                .to_string(),
+            "failed to send message: invalid socket address".to_string()
         );
 
         // timeout
         assert_eq!(
-            qtvusers_async("quake.se:28000", Duration::default())
+            query_qtvusers_async("quake.se:28000", Duration::default())
                 .await
-                .unwrap_err(),
-            QtvusersResponseError::UdpError(
-                "timeout reached while waiting for response".to_string()
-            )
+                .unwrap_err()
+                .to_string(),
+            "timeout reached while waiting for response".to_string()
         );
         Ok(())
     }
 
     #[test]
-    fn test_try_from() -> Result<()> {
+    fn test_parse_qtvusers_response() -> Result<()> {
         // invalid header
         assert_eq!(
-            QtvusersResponse::try_from(b"foo".as_slice()).unwrap_err(),
-            QtvusersResponseError::InvalidHeader
+            QtvusersResponse::parse(b"foo").unwrap_err(),
+            ParseError::InvalidHeader
         );
 
         // invalid body
         assert_eq!(
-            QtvusersResponse::try_from(b"\xff\xff\xff\xffnqtvusers ".as_slice()).unwrap_err(),
-            QtvusersResponseError::InvalidBody("No newline found".to_string())
+            QtvusersResponse::parse(b"\xff\xff\xff\xffnqtvusers ").unwrap_err(),
+            ParseError::InvalidBody("missing newline".to_string())
         );
-
-        // invalid stream id
         assert_eq!(
-            QtvusersResponse::try_from(b"\xff\xff\xff\xffnqtvusers foo\n".as_slice())
-                .unwrap_err()
-                .to_string(),
-            "Invalid stream id".to_string()
+            QtvusersResponse::parse(b"\xff\xff\xff\xffnqtvusers foo\n").unwrap_err(),
+            ParseError::InvalidBody("asdasdasd".to_string())
         );
 
         // no users
         {
-            let bytes = b"\xff\xff\xff\xffnqtvusers 1\n".as_slice();
-            let res = QtvusersResponse::try_from(bytes)?;
-            assert_eq!(res.stream_id(), 1);
-            assert!(res.client_names().as_slice().is_empty());
+            let response = QtvusersResponse::parse(b"\xff\xff\xff\xffnqtvusers 1\n")?;
+            assert_eq!(response.stream_id(), 1);
+            assert!(response.client_names().is_empty());
         }
 
         // has users
         {
-            let bytes = b"\xff\xff\xff\xffnqtvusers 12 \"[streambot]\" \"XantoM\"\n".as_slice();
-            let res = QtvusersResponse::try_from(bytes)?;
-            assert_eq!(res.stream_id(), 12);
+            let response = QtvusersResponse::parse(
+                b"\xff\xff\xff\xffnqtvusers 12 \"[streambot]\" \"XantoM\"\n",
+            )?;
+            assert_eq!(response.stream_id(), 12);
             assert_eq!(
-                res.client_names().as_slice(),
+                response.client_names(),
                 &["[streambot]".to_string(), "XantoM".to_string()]
             );
         }
