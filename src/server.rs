f01a8ac51196ac9911c846d60690994223095f58
Viktor Persson <viktor.persson@arcsin.se>
2025-05-27 18:37:26 +0200

    server: add methods for common attributes

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,4 @@
-use crate::{GameServer, GenericServer, ProxyServer, QtvServer};
+use crate::{GameServer, GenericServer, ProxyServer, QtvServer, ServerType, SoftwareType};
 
 #[derive(Debug, Clone, PartialEq)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
@@ -9,49 +9,49 @@ pub enum Server {
     Qtv(QtvServer),
 }
 
-// impl Server {
-//     fn server_type(&self) -> ServerType {
-//         match self {
-//             Server::Game(_) => ServerType::GameServer,
-//             Server::Generic(_) => ServerType::Unknown,
-//             Server::Proxy(_) => ServerType::ProxyServer,
-//             Server::Qtv(_) => ServerType::QtvServer,
-//         }
-//     }
+impl Server {
+    fn server_type(&self) -> ServerType {
+        match self {
+            Server::Game(_) => ServerType::GameServer,
+            Server::Generic(_) => ServerType::Unknown,
+            Server::Proxy(_) => ServerType::ProxyServer,
+            Server::Qtv(_) => ServerType::QtvServer,
+        }
+    }
 
-//     fn software_type(&self) -> SoftwareType {
-//         match self {
-//             Server::Game(s) => s.software_type(),
-//             Server::Generic(s) => s.software_type(),
-//             Server::Proxy(s) => s.software_type(),
-//             Server::Qtv(s) => s.software_type(),
-//         }
-//     }
+    fn software_type(&self) -> SoftwareType {
+        match self {
+            Server::Game(s) => s.software_type(),
+            Server::Generic(s) => s.software_type(),
+            Server::Proxy(s) => s.software_type(),
+            Server::Qtv(s) => s.software_type(),
+        }
+    }
 
-//     fn ip(&self) -> &str {
-//         match self {
-//             Server::Game(s) => s.ip(),
-//             Server::Generic(s) => s.ip(),
-//             Server::Proxy(s) => s.ip(),
-//             Server::Qtv(s) => s.ip(),
-//         }
-//     }
+    fn ip(&self) -> &str {
+        match self {
+            Server::Game(s) => s.ip(),
+            Server::Generic(s) => s.ip(),
+            Server::Proxy(s) => s.ip(),
+            Server::Qtv(s) => s.ip(),
+        }
+    }
 
-//     fn port(&self) -> u16 {
-//         match self {
-//             Server::Game(s) => s.port(),
-//             Server::Generic(s) => s.port(),
-//             Server::Proxy(s) => s.port(),
-//             Server::Qtv(s) => s.port(),
-//         }
-//     }
+    fn port(&self) -> u16 {
+        match self {
+            Server::Game(s) => s.port(),
+            Server::Generic(s) => s.port(),
+            Server::Proxy(s) => s.port(),
+            Server::Qtv(s) => s.port(),
+        }
+    }
 
-//     fn address(&self) -> &str {
-//         match self {
-//             Server::Game(s) => s.address(),
-//             Server::Generic(s) => s.address(),
-//             Server::Proxy(s) => s.address(),
-//             Server::Qtv(s) => s.address(),
-//         }
-//     }
-// }
+    fn address(&self) -> &str {
+        match self {
+            Server::Game(s) => s.address(),
+            Server::Generic(s) => s.address(),
+            Server::Proxy(s) => s.address(),
+            Server::Qtv(s) => s.address(),
+        }
+    }
+}
