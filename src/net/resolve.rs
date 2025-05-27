459dfc0811322c1e8b6a635a23afa3c198dc7caa
Viktor Persson <viktor.persson@arcsin.se>
2025-05-27 18:00:30 +0200

    improve test coverage for resolve

--- src/net/resolve.rs
+++ src/net/resolve.rs
@@ -30,15 +30,22 @@ pub mod tests {
 
     #[tokio::test]
     async fn test_host_as_ipv4() -> Result<()> {
+        // invalid
         assert_eq!(
             host_as_ipv4("INVALID_ADDRESS").unwrap_err(),
             Error::FailedToResolve("INVALID_ADDRESS".to_string())
         );
+
+        // ipv6
         assert_eq!(
-            host_as_ipv4("0:0:0:0:0:ffff:c0a8:0001:28000").unwrap_err(),
-            Error::FailedToResolve("0:0:0:0:0:ffff:c0a8:0001:28000".to_string())
+            host_as_ipv4("2606:4700:4700::1111").unwrap_err(),
+            Error::FailedToResolve("2606:4700:4700::1111".to_string())
         );
+
+        // ipv4
         assert_eq!(host_as_ipv4("1.2.3.4")?, "1.2.3.4".to_string());
+
+        // resolved ipv4
         assert!(
             ["1.1.1.1".to_string(), "1.0.0.1".to_string()]
                 .contains(&host_as_ipv4("one.one.one.one")?)
