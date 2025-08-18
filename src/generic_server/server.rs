f59b1517f07e3c3742df0f49aebb787058400c36
Viktor Persson <viktor.persson@arcsin.se>
2025-08-18 11:11:54 +0200

    update serialization of GenericServer

--- src/generic_server/server.rs
+++ src/generic_server/server.rs
@@ -1,9 +1,12 @@
 use crate::{GenericClient, GeoInfo, QtvStream, ServerType, SoftwareType};
 pub use quake_serverinfo::Settings;
 
+#[cfg(feature = "serde")]
+use serde::ser::SerializeStruct;
+
 /// Represents a server of unknown type.
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Hash, Default)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(feature = "serde", derive(serde::Deserialize))]
 pub struct GenericServer {
     pub(crate) server_type: ServerType,
     pub(crate) software_type: SoftwareType,
@@ -61,5 +64,27 @@ impl GenericServer {
 
     pub fn geo(&self) -> &GeoInfo {
         &self.geo
+    }
+}
+
+#[cfg(feature = "serde")]
+impl serde::Serialize for GenericServer {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let mut state = serializer.serialize_struct("GenericServer", 11)?;
+        state.serialize_field("server_type", &self.server_type())?;
+        state.serialize_field("software_type", &self.software_type())?;
+        state.serialize_field("address", &self.address())?;
+        state.serialize_field("ip", self.ip())?;
+        state.serialize_field("port", &self.port())?;
+        state.serialize_field("settings", self.settings())?;
+        state.serialize_field("client_count", &self.clients().len())?;
+        state.serialize_field("client_limit", &self.settings().maxclients)?;
+        state.serialize_field("clients", self.clients())?;
+        state.serialize_field("qtv_stream", &self.qtv_stream())?;
+        state.serialize_field("geo", self.geo())?;
+        state.end()
     }
 }
